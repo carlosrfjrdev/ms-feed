31001f0fc52637760a31e302cf5f6f7fb525d36e
Carlos Ferreira <carlosrfjr.dev@gmail.com>
2023-05-13 22:55:41 -0300

    inicial

---
+++ src/main/java/com/thinkhack/msfeed/MsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.thinkhack.msfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkhack/msfeed/MsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkhack.msfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
